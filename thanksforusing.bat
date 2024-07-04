31616701bf07a56d9938e5db31cfeab68492f62a
hndrxxbabymama <ixfive1@gmail.com>
2024-07-04 14:57:37 +0000

    Add files via upload

---
+++ thanksforusing.bat
@@ -0,0 +1,35 @@
+@echo off
+rmdir C:\TT\ >nul 2>&1
+color 5
+echo. 
+echo. 
+echo.
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                bse7tek tweak
+echo.                                                                           by.hndrxx tweaks
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+start https://discord.gg/GkSHyNSbw8
+(goto) 2>nul & del "%~f0"
